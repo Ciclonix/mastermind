f6d1774cc9239e8b01b5b4e7a0a1dfd3b006c548
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-08-23 14:25:32 +0200

    Add class Mastermind, add method play

--- mastermind.rb
+++ mastermind.rb
@@ -1,34 +1,56 @@
 # frozen_string_literal: true
 
-def randomNumberSequence
-  return Array.new(4) { rand(1, 7) }
-end
-
-def getInputSequence
-  puts "Try a combination:"
-  begin
-    player_try = gets.chomp.split("").map { |x| Integer(x) }
-    raise ArgumentError if player_try.size != 4 || player_try.min < 1 || player_try.max > 6
-  rescue ArgumentError
-    puts "Error: Invalid combination"
-    retry
+class Mastermind
+  def initialize
+    play ? puts("You win!") : puts("You lose!")
   end
-end
 
-def giveFeedback(original, try)
-  temp_og = original.clone
-  temp_try = try.clone
-  black = white = 0
-  temp_try.each_with_index do |num, idx|
-    if num == original[idx]
-      black += 1
-      temp_og[idx] = temp_try[idx] = nil
-    end
+  def randomNumberSequence
+    return Array.new(4) { rand(1..6) }
   end
-  temp_try.each do |num|
-    if !num.nil? && temp_og.include?(num)
-      white += 1
-      temp_og[temp_og.index(num)] = nil
+
+  def getInputSequence
+    begin
+      print "Try a combination: "
+      player_try = gets.chomp.split("").map { |x| Integer(x) }
+      raise ArgumentError if player_try.size != 4 || player_try.min < 1 || player_try.max > 6
+    rescue ArgumentError
+      puts "Error: Invalid combination"
+      retry
     end
+    return player_try
+  end
+
+  def giveFeedback(original, try)
+    temp_og = original.clone
+    temp_try = try.clone
+    feedback = {black: 0, white: 0}
+    temp_try.each_with_index do |num, idx|
+      if num == original[idx]
+        feedback[:black] += 1
+        temp_og[idx] = temp_try[idx] = nil
+      end
+    end
+    temp_try.each do |num|
+      if !num.nil? && temp_og.include?(num)
+        feedback[:white] += 1
+        temp_og[temp_og.index(num)] = nil
+      end
+    end
+    return feedback
+  end
+
+  def play
+    original = randomNumberSequence
+    12.times do |x|
+      x == 11 ? puts("1 try left") : puts("#{12 - x} tries left")
+      try = getInputSequence
+      feedback = giveFeedback(original, try)
+      return true if feedback[:black] == 4
+
+      puts "Correct position  #{feedback[:black]}"
+      puts "Wrong position    #{feedback[:white]}\n\n"
+    end
+    return false
   end
 end
